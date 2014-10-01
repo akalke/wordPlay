825c0104a71edc898b20eaeb19f85c6941a3b417
akalke <amaeya.kalke@gmail.com>
2014-10-01 03:32:49 -0500

    Updated comments to be more clear in ResultsVC

--- wordPlay/ResultsViewControllerViewController.m
+++ wordPlay/ResultsViewControllerViewController.m
@@ -21,37 +21,48 @@
 
     // Do any additional setup after loading the view.
 
-
-    //In the ResultsViewController, bold any of the words that user has entered using NSAttributedStrings
-    //hint: Use a NSMutableString and set that to the resultsTextView's attributedText property
-
-
-    //Set font to system font and bold
+    //SET FONT FORMAT
+    //Set font style for user input words. In this case, we are looking at the bold version of the system font
     UIFont *boldFont = [UIFont boldSystemFontOfSize:[UIFont systemFontSize]];
 
+
+    //BUILD PARAGRAPH STRINGS
     //Declare and initialize mutable strings that can take an attribute
-    NSMutableAttributedString *partOne = [[NSMutableAttributedString alloc] initWithString: @"The trip to "];
-    NSMutableAttributedString *partTwo = [[NSMutableAttributedString alloc] initWithString: @" was a(n) "];
-    NSMutableAttributedString *partThree = [[NSMutableAttributedString alloc] initWithString: @" vacation for "];
-    NSMutableAttributedString *partFour = [[NSMutableAttributedString alloc] initWithString: @" and the family ever took because they were able to "];
     NSMutableAttributedString *nameString = [[NSMutableAttributedString alloc]initWithString: self.name];
     NSMutableAttributedString *actionString = [[NSMutableAttributedString alloc]initWithString: self.action];
     NSMutableAttributedString *locationString = [[NSMutableAttributedString alloc]initWithString: self.location];
     NSMutableAttributedString *adjectiveString = [[NSMutableAttributedString alloc]initWithString: self.adjective];
 
+
+    //CONTINUE BUILDING PARAGRAPH STRINGS
+    //Need to build the static portions of the mad lib by using another set of mutable attributed strings. There is no
+    //way to pull the attributed strings into the traditional self.<UIObject>.text because this format will strip the
+    //format from the string
+    NSMutableAttributedString *partOne = [[NSMutableAttributedString alloc] initWithString: @"The trip to "];
+    NSMutableAttributedString *partTwo = [[NSMutableAttributedString alloc] initWithString: @" was a(n) "];
+    NSMutableAttributedString *partThree = [[NSMutableAttributedString alloc] initWithString: @" vacation for "];
+    NSMutableAttributedString *partFour = [[NSMutableAttributedString alloc] initWithString: @" and the family ever took because they were able to "];
+
+
+    //IDENTIFY RANGE
     //Define the length of each word to apply bold attribute to each word
     NSInteger nameLength = [self.name length];
     NSInteger actionLength = [self.action length];
     NSInteger locationLength = [self.location length];
     NSInteger adjectiveLength = [self.adjective length];
 
+
+    //APPLY FONT FORMAT
     //Add bold property to each word across the length of each user input word
     [nameString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0,nameLength)];
     [actionString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0,actionLength)];
     [locationString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0,locationLength)];
     [adjectiveString addAttribute:NSFontAttributeName value:boldFont range:NSMakeRange(0,adjectiveLength)];
 
-    //Build paragraph
+
+    //BUILD PARAGRAPH
+    //Append each individual string noted above to produce a final paragraph. In this case, we are building on top
+    //of partOne. Manual concatenation using the append method for attributed strings.
     [partOne appendAttributedString:locationString];
     [partOne appendAttributedString:partTwo];
     [partOne appendAttributedString:adjectiveString];
@@ -60,10 +71,14 @@
     [partOne appendAttributedString:partFour];
     [partOne appendAttributedString:adjectiveString];
 
+
+    //OUTPUT PARAGRAPH
     //Output paragraph with formatted input text to UITextField
     self.resultsTextView.attributedText = partOne;
 
-    //Original output statement
+
+    //ORIGINAL OUTPUT
+    //Unformatted version of the output leveraging typical object references %@ and variable names
     //self.resultsTextView.text = [NSString stringWithFormat:@"The trip to %@ was a(n) %@ vacation for %@ and the family ever took because they were able to %@", self.location, self.adjective, self.name, self.action];
 }
 
